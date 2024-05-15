ea709cc98e08fe315e8c728433de2e1f2078daa9
jason <jason@fortawesome.com>
2024-05-15 15:49:07 -0500

    first commmit

---
+++ app.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2>Font Awesome Icons</h2>
+    <hr />
+
+    <div>
+      <!-- Array Syntax -->
+      <font-awesome-icon
+        :icon="['fas', 'dog']"
+        size="3x"
+        shake
+      />
+
+      <!-- String Syntax -->
+      <font-awesome-icon
+        icon="fa-solid fa-cat"
+        size="2x"
+        bounce
+      />
+    </div>
+  </div>
+</template>
